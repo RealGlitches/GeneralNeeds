83b63cc9300953bea03112cd12d65df1f98b7b44
RealGlitches <BlueSanity@users.github.com>
2020-01-30 15:39:44 +0000

    put the bot online

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const Bot = new Discord.Client();
+
+const Token = 'NjcyNDYwNzI5NzA1ODI0Mjc2.XjL0Cw.PQ_lXgkq8OHvTfZagKx8uqShPB8';
+
+Bot.on('ready', () => {})
+    console.log('GernalNeeds is online!');
+
+Bot.login(Token);
